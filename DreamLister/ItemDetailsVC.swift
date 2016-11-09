482b1e98c34181d32cec4916eb7bb66da0986185
Flamur <flamurbeqiri1@gmail.com>
2016-11-09 14:41:27 +0100

    styled navigation

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -13,6 +13,9 @@ class ItemDetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
         // Do any additional setup after loading the view.
     }
 
